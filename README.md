af45b1098661548abfd4661a8b9676a9a1c55477
SCDevy <hello@stephenc.me>
2016-08-02 00:26:05 -0600

    Fixes indentation.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # angular2-seed
 A simple Angular 2/Typescript seed with some extra goodies.
+
+`npm install`
+
+`npm run clean & npm run build`
+
+`npm start`
--- gulpfile.ts
+++ gulpfile.ts
@@ -6,6 +6,7 @@ const tsc = require("gulp-typescript");
 const sourcemaps = require('gulp-sourcemaps');
 const tsProject = tsc.createProject("tsconfig.json");
 const tslint = require('gulp-tslint');
+const sass = require('gulp-sass');
 
 /** Remove build directory **/
 gulp.task('clean', (cb) => {
@@ -15,37 +16,37 @@ gulp.task('clean', (cb) => {
 /** Lint all custom TypeScript files **/
 gulp.task('tslint', () => {
     return gulp.src("src/**/*.ts")
-        .pipe(tslint())
-        .pipe(tslint.report('prose'));
+    .pipe(tslint())
+    .pipe(tslint.report('prose'));
 });
 
 /** Compile TypeScript sources and create sourcemaps in build directory **/
 gulp.task("compile", ["tslint"], () => {
     let tsResult = gulp.src("src/**/*.ts")
-        .pipe(sourcemaps.init())
-        .pipe(tsc(tsProject));
+    .pipe(sourcemaps.init())
+    .pipe(tsc(tsProject));
     return tsResult.js
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("build"));
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("build"));
 });
 
 /** Copy all resources that are not TypeScript files into build directory **/
 gulp.task("resources", () => {
     return gulp.src(["src/**/*", "!**/*.ts"])
-        .pipe(gulp.dest("build"));
+    .pipe(gulp.dest("build"));
 });
 
 /** Copy all required libraries into build directory **/
 gulp.task("libs", () => {
     return gulp.src([
-            'core-js/client/shim.min.js',
-            'zone.js/dist/zone.min.js',
-            'reflect-metadata/Reflect.js',
-            'systemjs/dist/system.js',
-            'rxjs/**',
-            '@angular/**'
-        ], {cwd: "node_modules/**"}) // Glob required here
-        .pipe(gulp.dest("build/lib"));
+        'core-js/client/shim.min.js',
+        'zone.js/dist/zone.min.js',
+        'reflect-metadata/Reflect.js',
+        'systemjs/dist/system.js',
+        'rxjs/**',
+        '@angular/**'
+    ], {cwd: "node_modules/**"}) // Glob required here
+    .pipe(gulp.dest("build/lib"));
 });
 
 /** Watch for changes in TypeScript, HTML and CSS files **/
