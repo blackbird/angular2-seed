104878db8eaae5dc121d5dee4df050a348fb2da2
SCDevy <hello@stephenc.me>
2016-10-12 17:12:43 -0700

    Moves index.scss to scss dir.

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('compileTs', ['tslint'], function() {
 
 // Compile SCSS sources
 gulp.task('compileScss', function() {
-	let scssResult = gulp.src('src/**/*.scss')
+	let scssResult = gulp.src('src/scss/**/*.scss')
 		.pipe(sassGlob())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer('last 2 versions'))
